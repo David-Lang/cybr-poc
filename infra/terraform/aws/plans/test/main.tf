bfc617e7617c0381a548b71e8bbbdb2cc5531b8e
David-Lang <16465823+David-Lang@users.noreply.github.com>
2023-11-28 14:17:35 -0500

    update

--- infra/terraform/aws/plans/test/main.tf
+++ infra/terraform/aws/plans/test/main.tf
@@ -6,7 +6,7 @@ variable "db_password" {
 data "aws_caller_identity" "current" {}
 
 data "conjur_secret" "aws_access_key" {
-  name = "data/vault/cybr-poc-terraform/cybr-poc-se-amer-aws/AWSAccountID"
+  name = "data/vault/cybr-poc-terraform/cybr-poc-se-amer-aws/AWSAccessKeyID"
 }
 
 data "conjur_secret" "aws_secret_key" {
