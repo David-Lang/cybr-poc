ab04fdd186771d2cfdd991e6b1309ad274d9057d
David-Lang <16465823+David-Lang@users.noreply.github.com>
2023-12-05 17:01:42 -0500

    update

--- infra/terraform/aws/plans/cpm/main.tf
+++ infra/terraform/aws/plans/cpm/main.tf
@@ -9,6 +9,9 @@ module "aws-vpc" {
   private_subnets = local.private_subnets
   # enable nat gateway for lambda traffic if needed
   enable_nat_gateway = false
+  terraform_cloud_api_key  = var.terraform_cloud_api_key
+  terraform_cloud_org_name = var.terraform_cloud_org_name
+  terraform_cloud_ws_id    = var.terraform_cloud_ws_id
 }
 
 module "aws-allowlist_sg" {
@@ -39,7 +42,7 @@ module "aws-compute-win" {
   keypair_name      = aws_key_pair.new-keypair.key_name
   lab_pwd           = var.lab_pwd
   name_prefix       = local.name_prefix
-  public_subnet_id  = module.aws-vpc.public_subnet_ids[0]
+  public_subnet_id  = module.aws-vpc.vpc_public_subnet_ids[0]
   vpc_cidr          = module.aws-vpc.vpc_cidr
   vpc_id            = module.aws-vpc.vpc_id
 }
