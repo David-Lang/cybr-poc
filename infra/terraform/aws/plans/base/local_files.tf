888c16293b6689e629b6ae27e3f96c8cc02bf823
David-Lang <16465823+David-Lang@users.noreply.github.com>
2024-01-31 12:08:48 -0500

    update

--- infra/terraform/aws/plans/base/local_files.tf
+++
@@ -1,6 +0,0 @@
-resource "local_file" "base_tfvars" {
-  content         = data.template_file.base_tfvars.rendered
-  filename        = "${path.module}/secrets.base.auto.tfvars.out"
-  file_permission = "0444"
-}
-
