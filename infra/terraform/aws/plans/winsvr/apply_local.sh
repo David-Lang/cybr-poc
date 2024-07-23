8598a3f76ea989ef8bde40fd7743aa5f87136278
David-Lang <16465823+David-Lang@users.noreply.github.com>
2024-07-23 15:14:30 -0700

    update

--- infra/terraform/aws/plans/winsvr/apply_local.sh
+++ infra/terraform/aws/plans/winsvr/apply_local.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
-isp_sub_domain="test-winsvr"
-terraform workspace new "$isp_sub_domain" 2> /dev/null || sleep 0
-terraform workspace select "$isp_sub_domain"
-terraform init
-terraform validate
-terraform plan -out="$isp_sub_domain".plan
-terraform apply -auto-approve "$isp_sub_domain".plan
+compute_name="test-winsvr"
+
+echo "Terraform will Apply $compute_name"
+echo "Do you want to proceed? (yes/no)"
+read -r answer
+
+# Convert the answer to lowercase
+answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
+
+# Check the user's answer
+if [ "$answer" == "yes" ]; then
+    echo "Proceeding..."
+
+    cp -f ../base/secrets.init.auto.tfvars secrets.init.auto.tfvars
+    cp -f ../base/secrets.base.auto.tfvars.out secrets.base.auto.tfvars
+
+    terraform workspace new "$compute_name" 2> /dev/null || sleep 0
+    terraform workspace select "$compute_name"
+    terraform init
+    terraform validate
+    terraform plan -out="$compute_name".plan
+    terraform apply -auto-approve "$compute_name".plan
+fi
