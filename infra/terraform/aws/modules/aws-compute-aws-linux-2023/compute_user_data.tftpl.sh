84ebf9a56b90162e29350a28222c1e966c592b54
David-Lang <16465823+David-Lang@users.noreply.github.com>
2025-09-25 13:35:57 -0700

    update

--- infra/terraform/aws/modules/aws-compute-aws-linux-2023/compute_user_data.tftpl.sh
+++ infra/terraform/aws/modules/aws-compute-aws-linux-2023/compute_user_data.tftpl.sh
@@ -5,10 +5,8 @@
 # shellcheck disable=SC2024
 
 log=/home/ec2-user/init_log
-sudo -u ubuntu touch $log
-sudo -u ubuntu echo "sudo -u ec2-user whoami: $(whoami)" >> $log 2>&1
-
-sudo apt --yes update >> $log 2>&1
+sudo -u ec2-user touch $log
+sudo -u ec2-user echo "sudo -u ec2-user whoami: $(whoami)" >> $log 2>&1
 
 # clone companion repo
 sudo -u ec2-user git clone --depth 1 --single-branch https://github.com/David-Lang/cybr-demos.git /home/ec2-user/cybr-demos
